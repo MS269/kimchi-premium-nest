db5f23f176cdb79c08c28dca64aeaefa8e165ce4
MS269 <ehddnr9797@gmail.com>
2024-01-31 19:18:48 +0900

    test: upbit

--- src/upbit/interfaces/orderbook-response.interface.ts
+++ src/upbit/interfaces/orderbook-response.interface.ts
@@ -1,4 +1,19 @@
 /** API: https://docs.upbit.com/reference/%ED%98%B8%EA%B0%80-%EC%A0%95%EB%B3%B4-%EC%A1%B0%ED%9A%8C */
+export interface UpbitOrderbookUnit {
+  /** 매도 호가 */
+  ask_price: number;
+
+  /** 매수 호가 */
+  bid_price: number;
+
+  /** 매도 잔량 */
+  ask_size: number;
+
+  /** 매수 잔량 */
+  bid_size: number;
+}
+
+/** API: https://docs.upbit.com/reference/%ED%98%B8%EA%B0%80-%EC%A0%95%EB%B3%B4-%EC%A1%B0%ED%9A%8C */
 export interface UpbitOrderbookResponse {
   /** 마켓 코드 (ex. KRW-BTC) */
   market: string;
@@ -13,19 +28,7 @@ export interface UpbitOrderbookResponse {
   total_bid_size: number;
 
   /** 호가 */
-  orderbook_units: {
-    /** 매도 호가 */
-    ask_price: number;
-
-    /** 매수 호가 */
-    bid_price: number;
-
-    /** 매도 잔량 */
-    ask_size: number;
-
-    /** 매수 잔량 */
-    bid_size: number;
-  }[];
+  orderbook_units: UpbitOrderbookUnit[];
 
   /** 호가 모아보기 단위 (default: 0, 기본 호가단위) */
   level: number;
--- src/upbit/upbit.service.spec.ts
+++ src/upbit/upbit.service.spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import axios from 'axios';
 
-import { UpbitOrderbookResponse } from './interfaces/orderbook-response.interface';
 import { UpbitSymbolResponse } from './interfaces/symbol-response.interface';
+import { PartialUpbitOrderbookResponse } from './types/orderbook-response.type';
 import { UpbitService } from './upbit.service';
 
 jest.mock('axios');
@@ -25,24 +25,18 @@ describe('UpbitService', () => {
   describe('getSymbols()', () => {
     it('should return all symbols which starts with KRW', async () => {
       // given
-      const res: UpbitSymbolResponse[] = [
+      const res: Partial<UpbitSymbolResponse>[] = [
         {
           market_warning: 'NONE',
           market: 'KRW-BTC',
-          korean_name: '비트코인',
-          english_name: 'Bitcoin',
         },
         {
           market_warning: 'NONE',
           market: 'KRW-ETH',
-          korean_name: '이더리움',
-          english_name: 'Ethereum',
         },
         {
           market_warning: 'NONE',
           market: 'BTC-ETH',
-          korean_name: '이더리움',
-          english_name: 'Ethereum',
         },
       ];
       jest.spyOn(axios, 'get').mockResolvedValue({ data: res });
@@ -51,12 +45,23 @@ describe('UpbitService', () => {
       const symbols = await service.getAllSymbols();
 
       // then
-      expect(symbols.length).toBe(res.length - 1);
+      const filtered: Partial<UpbitSymbolResponse>[] = [
+        {
+          market_warning: 'NONE',
+          market: 'KRW-BTC',
+        },
+        {
+          market_warning: 'NONE',
+          market: 'KRW-ETH',
+        },
+      ];
+
+      expect(symbols.length).toBe(filtered.length);
 
       symbols.map((symbol, i) =>
         expect(symbol).toEqual({
-          symbol: res[i].market,
-          warning: res[i].market_warning !== 'NONE',
+          symbol: filtered[i].market,
+          warning: filtered[i].market_warning !== 'NONE',
         }),
       );
     });
@@ -67,21 +72,15 @@ describe('UpbitService', () => {
       // given
       const symbols = ['KRW-BTC'];
 
-      const res: UpbitOrderbookResponse[] = [
+      const res: PartialUpbitOrderbookResponse[] = [
         {
           market: 'KRW-BTC',
-          timestamp: 1706687128273,
-          total_ask_size: 3.07995955,
-          total_bid_size: 1.08417531,
           orderbook_units: [
             {
               ask_price: 59101000,
               bid_price: 59100000,
-              ask_size: 0.13759379,
-              bid_size: 0.03418226,
             },
           ],
-          level: 0,
         },
       ];
       jest.spyOn(axios, 'get').mockResolvedValue({ data: res });
@@ -93,7 +92,7 @@ describe('UpbitService', () => {
       expect(orderbooks.length).toBe(symbols.length);
 
       expect(orderbooks[0]).toEqual({
-        symbol: res[0].market,
+        symbol: symbols[0],
         askPrice: res[0].orderbook_units[0].ask_price,
         bidPrice: res[0].orderbook_units[0].bid_price,
       });
@@ -103,36 +102,24 @@ describe('UpbitService', () => {
       // given
       const symbols = ['KRW-BTC', 'KRW-ETH'];
 
-      const res: UpbitOrderbookResponse[] = [
+      const res: PartialUpbitOrderbookResponse[] = [
         {
           market: 'KRW-BTC',
-          timestamp: 1706687266573,
-          total_ask_size: 2.59888689,
-          total_bid_size: 1.02068728,
           orderbook_units: [
             {
               ask_price: 59099000,
               bid_price: 59091000,
-              ask_size: 0.03092821,
-              bid_size: 0.28343708,
             },
           ],
-          level: 0,
         },
         {
           market: 'KRW-ETH',
-          timestamp: 1706687266639,
-          total_ask_size: 128.71361844,
-          total_bid_size: 206.36439601,
           orderbook_units: [
             {
               ask_price: 3216000,
               bid_price: 3214000,
-              ask_size: 1.71086115,
-              bid_size: 3.15299242,
             },
           ],
-          level: 0,
         },
       ];
       jest.spyOn(axios, 'get').mockResolvedValue({ data: res });
@@ -145,7 +132,7 @@ describe('UpbitService', () => {
 
       orderbooks.map((orderbook, i) =>
         expect(orderbook).toEqual({
-          symbol: res[i].market,
+          symbol: symbols[i],
           askPrice: res[i].orderbook_units[0].ask_price,
           bidPrice: res[i].orderbook_units[0].bid_price,
         }),
