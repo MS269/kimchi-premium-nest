9f516aa82a6546fe0af71b8b47a80a3e655e396e
MS269 <ehddnr9797@gmail.com>
2024-02-05 02:23:13 +0900

    fix: fetchPrices params and add logger

--- src/upbit/upbit.service.ts
+++ src/upbit/upbit.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import axios from 'axios';
 
 import { UpbitCoinResponse } from './interfaces/coin-response.interface';
@@ -7,44 +7,55 @@ import { UpbitPriceResponse } from './interfaces/price-response.interface';
 /** API: https://docs.upbit.com/reference */
 @Injectable()
 export class UpbitService {
+  private readonly logger = new Logger(UpbitService.name);
+
   /** API: https://docs.upbit.com/reference/%EB%A7%88%EC%BC%93-%EC%BD%94%EB%93%9C-%EC%A1%B0%ED%9A%8C */
   async fetchAllCoins() {
-    const { data } = await axios.get<UpbitCoinResponse[]>(
-      'https://api.upbit.com/v1/market/all',
-      { params: { isDetails: true } },
-    );
+    try {
+      const { data } = await axios.get<UpbitCoinResponse[]>(
+        'https://api.upbit.com/v1/market/all',
+        { params: { isDetails: true } },
+      );
 
-    const filtered = data.filter((coin) =>
-      coin.market.toUpperCase().startsWith('KRW'),
-    );
+      const filtered = data.filter((coin) =>
+        coin.market.toUpperCase().startsWith('KRW'),
+      );
 
-    return filtered.map((coin) => ({
-      exchange: 'Upbit',
-      name: coin.english_name,
-      symbol: coin.market.toUpperCase(),
-      baseAsset: coin.market.split('-')[1].toUpperCase(),
-      quoteAsset: coin.market.split('-')[0].toUpperCase(),
-      warning: coin.market_warning !== 'NONE',
-      message: coin.market_warning !== 'NONE' ? coin.market_warning : '',
-    }));
+      return filtered.map((coin) => ({
+        exchange: 'Upbit',
+        name: coin.english_name,
+        symbol: coin.market.toUpperCase(),
+        baseAsset: coin.market.split('-')[1].toUpperCase(),
+        quoteAsset: coin.market.split('-')[0].toUpperCase(),
+        warning: coin.market_warning !== 'NONE',
+        message: coin.market_warning !== 'NONE' ? coin.market_warning : '',
+      }));
+    } catch (error) {
+      this.logger.error(error);
+    }
   }
 
   /** API: https://docs.upbit.com/reference/%ED%98%B8%EA%B0%80-%EC%A0%95%EB%B3%B4-%EC%A1%B0%ED%9A%8C */
   async fetchPrices(symbols: string[]) {
-    if (symbols.length === 0) {
-      return [];
+    try {
+      if (symbols.length === 0) {
+        return [];
+      }
+
+      const { data } = await axios.get<UpbitPriceResponse[]>(
+        `https://api.upbit.com/v1/orderbook?markets=${symbols.join(
+          '&markets=',
+        )}`,
+      );
+
+      return data.map((orderbook) => ({
+        exchange: 'Upbit',
+        symbol: orderbook.market.toUpperCase(),
+        bidPrice: orderbook.orderbook_units[0].bid_price,
+        askPrice: orderbook.orderbook_units[0].ask_price,
+      }));
+    } catch (error) {
+      this.logger.error(error);
     }
-
-    const { data } = await axios.get<UpbitPriceResponse[]>(
-      'https://api.upbit.com/v1/orderbook',
-      { params: { markets: symbols.join('&markets=') } },
-    );
-
-    return data.map((orderbook) => ({
-      exchange: 'Upbit',
-      symbol: orderbook.market.toUpperCase(),
-      bidPrice: orderbook.orderbook_units[0].bid_price,
-      askPrice: orderbook.orderbook_units[0].ask_price,
-    }));
   }
 }
