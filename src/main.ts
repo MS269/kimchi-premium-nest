8f65c30de9ac868542dad76c8257a4890dba4e90
MS269 <ehddnr9797@gmail.com>
2024-02-10 18:06:13 +0900

    feat: port config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
+import 'dotenv/config';
+
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
+const PORT = Number(process.env.PORT) ?? 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
